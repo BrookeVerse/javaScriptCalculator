feda715f0c0ca1810c0ae25f3a1b31f3159ca432
BrookVerse <brookewaldorf90@gmail.com>
2022-08-16 15:49:08 +0100

    Added Grid, SCSS

---
+++ Java/mathFunctions.js
@@ -0,0 +1,29 @@
+//Function for addition
+const simpleAddition = (a, b) => {
+  return a + b;
+};
+console.log(simpleAddition(1, 2));
+
+//function for subtract
+const simpleSubtract = (a, b) => {
+  return a - b;
+};
+console.log(simpleSubtract(3, 1));
+
+//function for multiply
+const simpleMultiply = (a, b) => {
+  return a * b;
+};
+console.log(simpleMultiply(2, 2));
+
+//function for divide
+const simpleDivision = (a, b) => {
+  return a / b;
+};
+console.log(simpleDivision(10, 2));
+
+//function for percentage
+const simplePercentage = (a, b) => {
+  return (a / b) * 100;
+};
+console.log(simplePercentage(10, 100));
