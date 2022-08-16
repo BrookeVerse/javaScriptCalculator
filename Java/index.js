12cdaadf89946f133d81cb87dc94fa657eaa846b
BrookVerse <brookewaldorf90@gmail.com>
2022-08-16 18:43:30 +0100

    Bug Fixing HTML code, JS eventListening begining

---
+++ Java/index.js
@@ -0,0 +1,9 @@
+//Listening to which button is pressed, what type of button was pressed
+
+const calculator = document.querySelector(".calcuArea");
+const keys = calculator.querySelector(".calcuArea__buttons");
+
+keys.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+  }
+});
