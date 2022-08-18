5068fb7f1200e5ca4015417176f845e9b4daa26b
BrookVerse <brookewaldorf90@gmail.com>
2022-08-18 16:20:55 +0100

    Adding First QuerySelector and Event Listener

--- Java/index.js
+++ Java/index.js
@@ -1,9 +1,16 @@
-//Listening to which button is pressed, what type of button was pressed
+//Need collect all the numbers to create an event with them
+//const inputNumbers = document.querySelectorAll(".calcuArea__number");
+//console.log(inputNumbers);
 
-const calculator = document.querySelector(".calcuArea");
-const keys = calculator.querySelector(".calcuArea__buttons");
+const numbers = document.querySelectorAll(".calcuArea__number");
 
-keys.addEventListener("click", (e) => {
-  if (e.target.matches("button")) {
-  }
+let firstNumber = [];
+
+numbers.forEach((number) => {
+  number.addEventListener("Click", (event) => {
+    firstNumber.push(event.target.value);
+  });
 });
+console.log(firstNumber);
+
+//add event listener for each number that stores the value in the first number
